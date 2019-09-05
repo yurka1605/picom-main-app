23f42ecff7f3c1c2aa837c889edb33be7f3bb293
yurka1605 <litvinov.yuriy1996@gmail.com>
2019-09-05 16:15:26 +0400

    Build prod version

--- docs/modules/main/slider/slider.js
+++ docs/modules/main/slider/slider.js
@@ -3,10 +3,10 @@ import { sliderRightArrow, sliderLeftArrow, slides } from '../../_vars';
 
 export const sliderCheckEvents = () => {
     sliderLeftArrow.addEventListener('click', () => {
-
+        console.log('left');
     });
 
     sliderRightArrow.addEventListener('click', () => {
-        
+        console.log('right');
     });
 }
