25c79b938cfca7c56b0f2302c48f8e8e696f9c3c
yurka1605 <litvinov.yuriy1996@gmail.com>
2019-10-01 18:02:13 +0400

    fix bugs and started release slider

--- src/modules/main/slider/slider.js
+++ src/modules/main/slider/slider.js
@@ -1,12 +1,29 @@
-import { sliderRightArrow, sliderLeftArrow, slides } from '../../_vars';
+import { sliderRightArrow, sliderLeftArrow, slides, slide } from '../../_vars';
 
 
 export const sliderCheckEvents = () => {
     sliderLeftArrow.addEventListener('click', () => {
+        switchSlide(false);
         console.log('left');
     });
 
     sliderRightArrow.addEventListener('click', () => {
+        switchSlide(true);
         console.log('right');
     });
+
+    function switchSlide(isRight) {
+        let activeSlide;
+        slide.forEach(el => {
+            if (el.classList.contains('active')) {
+                activeSlide = el;
+            }
+        });
+
+        if (isRight) {
+
+        } else {
+
+        }
+    }
 }
