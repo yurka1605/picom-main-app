dbf641943c4eb0455e8c80ce770aed57b99933bf
yurka1605 <litvinov.yuriy1996@gmail.com>
2019-10-02 12:49:19 +0400

    slider release!

--- src/modules/main/slider/slider.js
+++ src/modules/main/slider/slider.js
@@ -1,16 +1,39 @@
-import { sliderRightArrow, sliderLeftArrow, slides, slide } from '../../_vars';
+import { sliderRightArrow, sliderLeftArrow, slider, slides, slide } from '../../_vars';
 
 
 export const sliderCheckEvents = () => {
-    sliderLeftArrow.addEventListener('click', () => {
-        switchSlide(false);
-        console.log('left');
-    });
+    console.log('ontouchstart' in window);
+    if (!('ontouchstart' in window)) {
+        sliderLeftArrow.addEventListener('click', () => {
+            switchSlide(false);
+        });
+    
+        sliderRightArrow.addEventListener('click', () => {
+            switchSlide(true);
+        });
+    } else {
+        let startX;
+        slider.addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            startX = e.screenX;
+        });
 
-    sliderRightArrow.addEventListener('click', () => {
-        switchSlide(true);
-        console.log('right');
-    });
+        slider.addEventListener('touchend', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            e.screenX;
+            let touchMoveX = e.screenX - startX;
+
+            if (touchMoveX > -10 ) {
+                switchSlide(false);
+            } else if (touchMoveX < 10) {
+                switchSlide(true);
+            } else {
+                return false;
+            }
+        });
+    }
 
     function switchSlide(isRight) {
         let activeSlide;
@@ -21,9 +44,30 @@ export const sliderCheckEvents = () => {
         });
 
         if (isRight) {
+            const nextSlide = activeSlide.nextElementSibling;
 
+            if (!nextSlide) {
+                slides.firstElementChild.classList.add('active');
+                slides.firstElementChild.classList.remove('active-right');
+                slides.appendChild(slides.firstElementChild);
+            } else {
+                nextSlide.classList.add('active');
+            }
+            activeSlide.classList.add('active-right');
+            activeSlide.classList.remove('active');
         } else {
-
+            const prevSlide = activeSlide.previousElementSibling;
+            if (!prevSlide) {
+                slides.lastElementChild.classList.add('active-right','active');
+                slides.insertBefore(slides.lastElementChild, slides.firstElementChild);
+                setTimeout(() => {
+                    slides.firstElementChild.classList.remove('active-right');
+                }, 0);
+            } else {
+                prevSlide.classList.remove('active-right');
+                prevSlide.classList.add('active');
+            }
+            activeSlide.classList.remove('active');
         }
     }
 }
